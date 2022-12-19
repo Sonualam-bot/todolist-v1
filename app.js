cd3adf6b7d84a8a66e0c873a1610a957904aeb1c
Sonu <sonualam1599@gmail.com>
2022-12-19 21:05:20 +0530

    update: connected with mongoose db
    connected the app with mongoose and added database items into the app

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//requiring the date.js file
-const date = require(__dirname + "/date.js");
+
+
+//now require mongoose package
+const mongoose = require("mongoose");
 
 //here we are requring our app through express
 const app = express();
 
-const items = ["But Food", "Cook Food", "Eat Food"];
 
-//empty array to store work route entries
-const workItems = [];
+
 
 //using ejs view engine in our site
 app.set("view engine", "ejs");
@@ -22,15 +22,72 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //to tell express to serve styles.css file as a static resource
 app.use(express.static("public"));
 
+//Now to connect here with a database
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useNewUrlParser: true
+});
+
+//the items schema 
+const itemsSchema = {
+    name: String,
+
+};
+
+//mongoose model to implement the schema above
+const Item = mongoose.model("Item", itemsSchema);
+
+//mongoose items model
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "Hit this to delete an item"
+});
+
+//now to add all the items into a common array
+const defaultItems = [item1, item2, item3];
+
+
+// to insert all this into the db in one go
+// Item.insertMany(defaultItems, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully saved default items to DB.");
+//     }
+// });
+
+
 //here we are defining the route location for our site
 app.get("/", function (req, res) {
 
-    const day = date.getDate();
 
+    Item.find({}, function (err, foundItems) {
+
+        if (foundItems.length === 0) {
+            // to insert all this into the db in one go
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved default items to DB.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Today", newListItems: foundItems });
+        }
+
+
+
+    });
 
     //placing this code here ensures that after a lot of logic that is being run above only after the logic the message will be loaded
-    res.render("list", { listTitle: day, newListItems: items });;
-
 });
 
 
